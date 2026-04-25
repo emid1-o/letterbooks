e47385c9b6c3d14609a7ac7e34f1d656e43bbfca
emid1o <emidiolop5@gmail.com>
2026-04-25 19:13:10 -0300

    adiciona controller e rota

---
+++ app/Http/Controllers/BookLogController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookLog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class BookLogController extends Controller
+{
+    public function index()
+    {
+        $books = BookLog::where('user_id', Auth::id())->orderByDesc('read_date')->get();
+        return view('booklogs.index', compact('books'));
+    }
+
+    public function create()
+    {
+        return view('booklogs.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'rating' => 'nullable|integer|min:1|max:5',
+            'review' => 'nullable|string',
+            'read_date' => 'nullable|date',
+        ]);
+
+        $validated['user_id'] = Auth::id();
+        BookLog::create($validated);
+
+        return redirect()->route('booklogs.index');
+    }
+
+    public function destroy(BookLog $booklog)
+    {
+        if ($booklog->user_id === Auth::id()) {
+            $booklog->delete();
+        }
+        return redirect()->route('booklogs.index');
+    }
+}
